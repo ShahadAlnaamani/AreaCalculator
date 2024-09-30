bf514af7c2c3482d034b9dc754e4c0dec43978ce
Shahad <Shahadalnaamani@outlook.com>
2024-09-30 16:59:54 +0400

    Completed functionality :)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AreaCalculator
+﻿using System.Numerics;
+using System.Reflection;
+
+namespace AreaCalculator
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("- - -A R E A   C A L C U L A T O R- - -");
+            shape cir = new Circle(4,5);
+            shape rec = new Rectangle(4, 5);
+            shape tri = new Triangle(4, 5);
+            shape squ = new Square(4, 5);
 
+            double CircleArea = cir.Area();
+            Console.WriteLine($"Circle area: {CircleArea}\n");
+            
+            double RectangleArea = rec.Area();
+            Console.WriteLine($"Rectangle area: {RectangleArea}\n");
+            
+            double TriangleArea = tri.Area();
+            Console.WriteLine($"Triangle area: {TriangleArea} \n");
+
+            double SquareArea = squ.Area();
+            Console.WriteLine($"Square area: {SquareArea} \n");
         }
     }
 
@@ -22,7 +41,7 @@
         }
 
         //Method
-        public virtual double Area(double length, double width)
+        public virtual double Area()
         { return 0; }
     }
 
@@ -32,10 +51,12 @@
         public Square(double length, double width) : base(length, width) { }
 
         //method 
-        public override double Area(double Length, double width) 
+        public override double Area() 
         {
             double Area = 0; 
-            Area += Math.Pow(Length, 2);
+            Area = Math.Pow(Length, 2);
+
+            Console.WriteLine("Calculating area of a square...");
 
             return Area;
         }
@@ -44,15 +65,57 @@
 
     public class Circle : shape
     {
+        //Constructor
+        public Circle(double length, double width) : base(length, width) { }
+
+        //method 
+        public override double Area()
+        {
+            //Consider width is r*2 (full line through circle)
+            double Area = 0;
+            double Radius = 0;
+            Radius = Width/2;
+            Area = Math.PI*(Math.Pow(Radius,2));
+
+            Console.WriteLine("Calculating area of a circle...");
+
+            return Area;
+        }
     }
 
     public class Triangle : shape
     {
+        //Constructor
+        public Triangle(double length, double width) : base(length, width) { }
 
+        //method 
+        public override double Area()
+        {
+            //Consider Length is hight and width is base
+            double Area = 0;
+            Area = (1/2 * (Length * Width));
+
+            Console.WriteLine("Calculating area of a triangle...");
+
+            return Area;
+        }
     }
 
     public class Rectangle : shape
     {
+        //Constructor
+        public Rectangle(double length, double width) : base(length, width) { }
+
+        //method 
+        public override double Area()
+        {
+            double Area = 0;
+            Area = Length*Width;
+
+            Console.WriteLine("Calculating area of a rectangle...");
+
+            return Area;
+        }
     }
 
 }
