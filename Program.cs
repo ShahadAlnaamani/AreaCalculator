938ac366a7899c14894264c7e1b13e728bcefb7d
Shahad <Shahadalnaamani@outlook.com>
2024-09-30 16:27:03 +0400

    Added functionality to square area calculator

--- Program.cs
+++ Program.cs
@@ -1,30 +1,58 @@
 ﻿namespace AreaCalculator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
-        }
 
-        public class shape
+        }
+    }
+
+    public abstract class shape
+    {
+        //Attributes
+        protected double Length { get; private set; }
+        protected double Width { get; private set; }
+
+        //Constructor 
+        public shape(double length, double width)
         {
-            //Attributes
-            protected double Length;
-            protected double Width;
-
-            //Constructor 
-            public shape(double length, double width)
-            {
-                Length = length;
-                Width = width;  
-            }
-
-            //Method
-            public virtual double area()
-            { return 0;  }
+            Length = length;
+            Width = width;
         }
 
+        //Method
+        public virtual double Area(double length, double width)
+        { return 0; }
+    }
+
+    public class Square : shape
+    {
+        //Constructor
+        public Square(double length, double width) : base(length, width) { }
+
+        //method 
+        public override double Area(double Length, double width) 
+        {
+            double Area = 0; 
+            Area += Math.Pow(Length, 2);
+
+            return Area;
+        }
         
     }
+
+    public class Circle : shape
+    {
+    }
+
+    public class Triangle : shape
+    {
+
+    }
+
+    public class Rectangle : shape
+    {
+    }
+
 }
